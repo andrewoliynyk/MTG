6b5cd00cd26a32696f03592dcd2a80bd4834dce3
Andrii Oliinyk <andriy.v.oliynyk@gmail.com>
2018-06-17 16:46:36 +0300

    Add project files.

---
+++ MTG/Controllers/CardController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MTG.Models;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MTG.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Cards")]
+    public class CardsController : Controller
+    {
+        private readonly CardContext _context;
+
+        public CardsController(CardContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cards
+        [HttpGet]
+        public IEnumerable<Card> GetCards()
+        {
+            return _context.Cards;
+        }
+
+        // GET: api/Cards/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCard([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var Card = await _context.Cards.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (Card == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(Card);
+        }
+
+        // PUT: api/Cards/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCard([FromRoute] string id, [FromForm] Card Card)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != Card.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(Card).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CardExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Cards
+        [HttpPost]
+        public async Task<IActionResult> PostCard([FromForm] Card Card)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Cards.Add(Card);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCard", new { id = Card.Id }, Card);
+        }
+
+        // DELETE: api/Cards/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCard([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var Card = await _context.Cards.SingleOrDefaultAsync(m => m.Id == id);
+            if (Card == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cards.Remove(Card);
+            await _context.SaveChangesAsync();
+
+            return Ok(Card);
+        }
+
+        private bool CardExists(string id)
+        {
+            return _context.Cards.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ MTG/Models/CardContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MTG.Models
+{
+    public class CardContext : DbContext
+    {
+        public CardContext(DbContextOptions<CardContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Card> Cards { get; set; }
+    }
+}
